11dfcad50a120a817ac55a2071a2472a74d49938
Grafe <flingelfrank@hotmail.com>
2013-02-21 21:08:40 +0100

    Removed characters folder Removed Network class
    Signed-off-by: Grafe <flingelfrank@hotmail.com>

--- src/com/dre/projectxserver/net/NetServer.java
+++ src/com/dre/projectxserver/net/NetServer.java
@@ -3,16 +3,25 @@ package com.dre.projectxserver.net;
 
 import java.io.IOException;
 
+import com.dre.projectxserver.net.packages.PlayerDisconnectPackage;
+import com.dre.projectxserver.net.packages.PlayerLoginPackage;
+import com.dre.projectxserver.net.packages.PlayerNewPackage;
+import com.dre.projectxserver.net.packages.PlayerPositionPackage;
+import com.dre.projectxserver.net.packages.PlayerVisiblePackage;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.EndPoint;
 import com.esotericsoftware.kryonet.Server;
 
 public class NetServer {
-	Server server;
+	public Server server;
+
+	public static Kryo kryo;
 
 	public NetServer (){
 		server = new Server();
 
 		// Register the classes that will be sent over the network.
-		Network.register(server);
+		registerKryoClasses(server);
 
 		// Add listener
 		server.addListener(new NetListener());
@@ -26,5 +35,19 @@ public class NetServer {
 
 		//Start server
 		server.start();
+	}
+
+	//Kryo
+	public static void registerKryoClasses (EndPoint endPoint) {
+		kryo = endPoint.getKryo();
+
+		//Initialize kryo classes
+		kryo.register(PlayerLoginPackage.class);
+		kryo.register(PlayerPositionPackage.class);
+		kryo.register(PlayerVisiblePackage.class);
+		kryo.register(PlayerNewPackage.class);
+		kryo.register(PlayerDisconnectPackage.class);
+
+		//TODO: Automatisieren
 	}
 }
